2de102c304304d69a3292ecc8b258bb7ad647452
Dewranga-Bandara <dewrangabandara071@gmail.com>
2025-09-19 03:21:58 +0530

    Integrate clerk authentication with the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,30 @@
+'use client';
 import Link from 'next/link';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-3 max-w-6xl mx-auto
-                       bg-white dark:bg-gray-900
-                       text-gray-900 dark:text-gray-100
-                       transition-colors duration-300">
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
       <ul className="flex gap-4">
-        <li><Link href="/sign-in">Sign in</Link></li>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <li><Link href="/sign-in">Sign in</Link></li>
+        </SignedOut>
         <li className="hidden sm:block"><Link href="/">Home</Link></li>
         <li className="hidden sm:block"><Link href="/about">About</Link></li>
       </ul>
-      <div className="flex items-center gap-4">
+      <div className='flex items-center gap-4'>
         <DarkModeSwitch />
-        <Link href="/" className="flex gap-1 items-center">
-          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">Cine</span>
-          <span className="text-xl hidden sm:inline">Mix</span>
+        <Link href={'/'} className='flex gap-1 items-center'>
+          <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>
+            Cine
+          </span>
+          <span className='text-xl hidden sm:inline'>Mix</span>
         </Link>
       </div>
-    </header>
+    </div>
   );
 }

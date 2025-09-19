530b42e2617bfc537dd57145a039cdd59255865c
Dewranga-Bandara <dewrangabandara071@gmail.com>
2025-09-20 01:06:08 +0530

    Add API route for fetching home page content and update page component to display it

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,55 @@
 import Results from '@/components/Results';
-
-export const dynamic = 'force-dynamic'; // prevents static build
-
+const API_KEY = process.env.API_KEY;
 export default async function Home() {
-  const API_KEY = process.env.API_KEY;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+  let homePageContent = null;
+  try {
+    const results = await fetch(process.env.URL + '/api/homepagecontent/get', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-store',
+    });
 
-  if (!API_KEY) {
-    console.error('API_KEY is missing');
-    return <div>API Key is missing</div>;
+    if (!results.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const text = await results.text();
+    if (text) {
+      const data = JSON.parse(text);
+      homePageContent = data[0] || null;
+    } else {
+      console.log('Empty response');
+    }
+  } catch (error) {
+    console.log('Error getting the home page content', error);
   }
 
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US&page=1`
-    );
-
-    if (!res.ok) throw new Error('Failed to fetch data');
-
-    const data = await res.json();
-    const results = data.results;
-
-    return (
+  return (
+    <div>
+      {homePageContent && (
+        <div className='text-center mb-10 max-w-6xl mx-auto py-10'>
+          <h1 className='text-4xl font-extrabold mb-4 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent'>
+            {homePageContent.title}
+          </h1>
+          <div
+          className='sm:text-lg p-4'
+            dangerouslySetInnerHTML={{ __html: homePageContent.description }}
+          ></div>
+        </div>
+      )}
       <div>
         <Results results={results} />
       </div>
-    );
-  } catch (err) {
-    console.error(err);
-    return <div>Failed to load data</div>;
-  }
+    </div>
+  );
 }
